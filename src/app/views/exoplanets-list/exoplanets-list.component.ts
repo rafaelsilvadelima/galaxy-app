a9444974f2575dcbe909dc413c77e3021449741c
Rafael Silva de Lima <rafaelsilvadelimaoficial@gmail.com>
2022-11-15 22:26:05 -0300

    update_15_11_2.0

--- src/app/views/exoplanets-list/exoplanets-list.component.ts
+++ src/app/views/exoplanets-list/exoplanets-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Exoplanet } from 'src/app/models/exoplanet';
@@ -9,13 +9,18 @@ import { ExoplanetService } from 'src/app/services/exoplanet.service';
   templateUrl: './exoplanets-list.component.html',
   styleUrls: ['./exoplanets-list.component.css']
 })
-export class ExoplanetsListComponent implements OnInit{
+export class ExoplanetsListComponent implements OnInit, AfterViewInit{
 
     constructor(private exoplanetService: ExoplanetService) {}
 
     public exoplanets: Exoplanet[] = []
     displayedColumns: string[] = ['name', 'stars', 'planets', 'moons', 'year', 'distance'];
     dataSource = new MatTableDataSource<Exoplanet>(this.exoplanets);
+    
+    ngAfterViewInit(){
+      /*this.dataSource = new MatTableDataSource<Exoplanet>(this.exoplanets);
+      this.dataSource.paginator = this.paginator;*/
+    }
   
     @ViewChild(MatPaginator) paginator?: MatPaginator;
   
@@ -24,9 +29,7 @@ export class ExoplanetsListComponent implements OnInit{
       this.exoplanetService.getAll().subscribe(exoplanets => {
         this.exoplanets = exoplanets;
         this.dataSource = new MatTableDataSource<Exoplanet>(this.exoplanets);
-      }
-        ) 
         if(this.paginator){
           this.dataSource.paginator = this.paginator;
-        }}
-  }
+        }});
+  }}
